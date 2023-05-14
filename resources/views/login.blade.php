35d31b9d0c802cfb9cc607b75f0194acfef99e6d
ViliamRideky <rideky.viliam@gmail.com>
2023-05-14 09:36:56 +0200

    mala uprava frontendu formularov

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,15 +5,15 @@
   <h1 class="text-center">Prihlásenie</h1>
   <form id="form" class="my-form row g-3" method="post" action="#">
     @csrf
-    <div class="col-md-6">
+    <div class="col-md-12 form-group">
       <label for="lastName" class="form-label">Email</label>
       <input type="text" name="email" class="form-control" required>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-12 form-group">
       <label for="birthDay" class="form-label">Heslo</label>
       <input type="password" name="password" class="form-control" required>
     </div>
-    <button type="submit" class="btn btn-primary col-md-12">Prihlásiť sa</button>
+    <button type="submit" class="btn btn-info col-md-12">Prihlásiť sa</button>
   </form>    
 </div>
 @endsection
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -5,27 +5,27 @@
   <h1 class="text-center">Registrácia</h1>
   <form id="form" class="my-form row g-3">
     @csrf
-    <div class="col-md-6">
+    <div class="col-md-12 form-group">
       <label for="firstName" class="form-label">Meno</label>
       <input id="rFirstName" type="text" class="form-control" required>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-12 form-group">
       <label for="lastName" class="form-label">Priezvisko</label>
       <input id="rLastName" type="text" class="form-control" required>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-12 form-group">
       <label for="rEmail" class="form-label ">Email</label>
       <input id="rEmail" type="text" class="form-control" required>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-12 form-group">
       <label for="rPassword" class="form-label">Heslo</label>
       <input id="rPassword" type="password" class="form-control" required>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-12 form-group">
       <label for="rPasswordConfirmation" class="form-label">Potvrdenie hesla</label>
       <input id="rPasswordConfirmation" type="password" class="form-control" required>
     </div>
-    <button type="button" onclick="submitForm()" class="btn btn-primary col-md-12">Registrovať sa</button>
+    <button type="button" onclick="submitForm()" class="btn btn-info col-md-12">Registrovať sa</button>
   </form>   
 </div>
 <script src="{{ asset('js/registration.js') }}"></script>
