2a341f32aad4757cdba208464e40c7e591b478f3
andrejzak <andyzak12345@gmail.com>
2023-05-16 09:59:45 +0200

    pridanie faviconu

--- resources/views/guest-layout.blade.php
+++ resources/views/guest-layout.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset("css/style.css") }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('icon.png') }}">
     <!-- Include Flag Icon CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
